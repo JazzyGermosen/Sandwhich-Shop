af22d7e8199e43304203cc1e30488fbb96ea08bb
jazzy <jazzygermosen@gmail.com>
2025-09-29 13:53:09 -0400

    i have finished the sandwhich shop!!!

--- src/main/java/com/Sandwichapp.java
+++ src/main/java/com/Sandwichapp.java
@@ -8,10 +8,11 @@ public class Sandwichapp {
         Scanner sandwich = new Scanner(System.in);
         // ask for the sandwhich size 'regular or large '
         System.out.print("What size sandwhich do you want? ");
-        System.out.println("Regular: base price $5.45");
-        System.out.println("Large: base price $8.95");
+        System.out.println("1. Regular: base price $5.45");
+        System.out.println("2. Large: base price $8.95");
 
         int size = sandwich.nextInt();
+//        System.out.println("size: " + size);
 
 
         double regular = 5.45;
@@ -25,21 +26,29 @@ public class Sandwichapp {
         // if student is 17 or younger they get a 10 percent discount
         String command = sandwich.nextLine();
         if (size == 1){
+            finalPrice = regular;
 
+        }
+        else if (size == 2) {
+            finalPrice = large;
+        }
+        else {
+            System.out.println("error: " + size + " is not an option");
         }
 
 
-        age <= 17 {
+
+        if ( age <= 17 ) {
+            finalPrice = finalPrice  - (finalPrice * .1);
             System.out.println("You get a 10% discount");
             }
         // if age is 65 or older you recieve a 20% discount
-        else if age >= 65 {
+        else if ( age >= 65 ) {
+            finalPrice = finalPrice - (finalPrice * .2);
             System.out.println("You get a 20% discount");
         }
 
-        else {
-
-        }
+        System.out.println("final price is: " + finalPrice);
         /*
          I know i need to apply the discount and that i need to
          */
